21047fb78176552ea09b9f2ee4b31dec560398d3
bmehder <bmehder@gmail.com>
2022-12-22 11:41:18 +0000

    modified stroop-locals.css

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,7 +1,8 @@
 import type { RequestEvent } from '@sveltejs/kit'
 import type { WP_REST_API_Posts } from 'wp-types'
 
-export async function load({ fetch, params }: RequestEvent) {
+/** @type {import('./$types').PageLoad} */
+export const load = async ({ fetch, params }: RequestEvent) => {
   const response = await fetch(
     `https://sprucehealthgroup.com/wp-json/wp/v2/posts?slug=${params.slug}&_embed`
   )
